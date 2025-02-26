4ffdf481f2dd84e0db8b1b5a35f5f5cfd3feef34
yijordan <yij@uchicago.edu>
2025-02-26 00:20:27 +0000

    added horizontal navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
+  get("/", { :controller => "dice", :action => "homepage"})
 
-get("/", { :controller => "dice"; :action => "homepage"})
+    # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
-  # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
-
-  # get "/your_first_screen" => "pages#first"
-  
+    # get "/your_first_screen" => "pages#first"
 end
