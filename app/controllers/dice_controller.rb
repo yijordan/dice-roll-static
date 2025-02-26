ae7df3d66f4b5d4393168d6cbe5a5b455c1f72f9
yijordan <yij@uchicago.edu>
2025-02-26 00:32:11 +0000

    started building the dynamic

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -2,4 +2,8 @@ class DiceController < ApplicationController
   def homepage
     render({ :template => "dice_templates/homepage"})
   end
+
+  def roll
+    render({ :template => "dice_templates/roll"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
     # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
     # get "/your_first_screen" => "pages#first"
+  
+  get("/dice/:number_of_dice/:how_many_sides", { :controller => "dice", :action => "roll" })
 end
