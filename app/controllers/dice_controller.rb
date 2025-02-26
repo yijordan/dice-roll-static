9ae8a33a24064e786030848524ce652060b5ed73
yijordan <yij@uchicago.edu>
2025-02-26 00:36:17 +0000

    did the params fetch

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,6 +4,14 @@ class DiceController < ApplicationController
   end
 
   def roll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+    @num_dice.times do
+      die = rand(1..@sides)
+      @rolls.push(die)
+    end
+
     render({ :template => "dice_templates/roll"})
   end
 end
